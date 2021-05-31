7d55f506116599c4dbae67d6b75c07193095dbd3
Erle Monfils <erlemonfils@Erles-MBP.wind3.hub>
2021-05-31 13:10:17 +0200

    use astronomical distance

--- src/shared/API/getAsteroids.js
+++ src/shared/API/getAsteroids.js
@@ -12,7 +12,7 @@ function getAsteroids(startDate, endDate) {
           diameter: +(+(estimated_diameter.kilometers.estimated_diameter_max + estimated_diameter.kilometers.estimated_diameter_min) / 2)
             .toFixed(2),
           velocity: +(+close_approach_data[0].relative_velocity.kilometers_per_second).toFixed(2),
-          distance: +close_approach_data[0].miss_distance.kilometers,
+          distance: +close_approach_data[0].miss_distance.astronomical,
         }))
 
         return {...prevVal, [date]: newAsteroids }
